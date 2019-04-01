f4c23d833c35229d9e9bfb1a13ad297be0c015c2
zhang5618 <137262314@qq.com>
2019-04-01 18:06:52 +0800

    大祥啊   你赶紧看一下啊！

--- src/com/kgc/pojo/Book.java
+++ src/com/kgc/pojo/Book.java
@@ -3,89 +3,93 @@ package com.kgc.pojo;
 import java.sql.Date;
 
 /**
- * 我就是梁大祥  今天在这里说了！   没有我找不到的bug   就是这么叼    狂拽酷炫吊炸天 666
+ * 我张宙   今天在这里说了！   没有我找不到的bug   就是这么叼    狂拽酷炫吊炸天 666
  */
 public class Book {
-   private int id;
-   private String name;
-   private String author;
-   private Date saleTime;
-   private double price;
-   private int type;
+    private int id;
+    private String name;
+    private String author;
+    private Date saleTime;
+    private double price;
+    private int type;
 
-   public Book() {
-   }
+    public void test2() {
+        System.out.println();
+    }
 
-   public Book(int id, String name, String author, Date saleTime, double price, int type) {
-      this.id = id;
-      this.name = name;
-      this.author = author;
-      this.saleTime = saleTime;
-      this.price = price;
-      this.type = type;
-   }
+    public Book() {
+    }
 
-   public int getId() {
-      return id;
-   }
+    public Book(int id, String name, String author, Date saleTime, double price, int type) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.saleTime = saleTime;
+        this.price = price;
+        this.type = type;
+    }
 
-   public void setId(int id) {
-      this.id = id;
-   }
+    public int getId() {
+        return id;
+    }
 
-   public String getName() {
-      return name;
-   }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-   public void setName(String name) {
-      this.name = name;
-   }
+    public String getName() {
+        return name;
+    }
 
-   public String getAuthor() {
-      return author;
-   }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-   public void setAuthor(String author) {
-      this.author = author;
-   }
+    public String getAuthor() {
+        return author;
+    }
 
-   public Date getSaleTime() {
-      return saleTime;
-   }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
-   public void setSaleTime(Date saleTime) {
-      this.saleTime = saleTime;
-   }
+    public Date getSaleTime() {
+        return saleTime;
+    }
 
-   public double getPrice() {
-      return price;
-   }
+    public void setSaleTime(Date saleTime) {
+        this.saleTime = saleTime;
+    }
 
-   public void setPrice(double price) {
-      this.price = price;
-   }
+    public double getPrice() {
+        return price;
+    }
 
-   public int getType() {
-      return type;
-   }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
-   public void setType(int type) {
-      this.type = type;
-   }
+    public int getType() {
+        return type;
+    }
 
-   @Override
-   public String toString() {
-      return "Book{" +
-              "id=" + id +
-              ", name='" + name + '\'' +
-              ", author='" + author + '\'' +
-              ", saleTime=" + saleTime +
-              ", price=" + price +
-              ", type=" + type +
-              '}';
-   }
+    public void setType(int type) {
+        this.type = type;
+    }
 
-   public String toString1() {
-      return name + "," + author + "," + saleTime + "," + price + "," + type + "," + id;
-   }
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", saleTime=" + saleTime +
+                ", price=" + price +
+                ", type=" + type +
+                '}';
+    }
+
+    public String toString1() {
+        return name + "," + author + "," + saleTime + "," + price + "," + type + "," + id;
+    }
 }
